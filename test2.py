9eaddbafe42ab4e14fa33adff5548d0a364d5506
Avetvarazdat <avetvarazdat@gmail.com>
2023-01-24 14:47:25 +0400

    asdf

--- test2.py
+++ test2.py
@@ -1,3 +1,5 @@
 print("This is a p2")
 print("This is a comment for testing purpose")
 print("This is a comment for testing purpose")
+print("This is a comment for testing purpose")
+print("This is a comment for testing purpose")
